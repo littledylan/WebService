3e2598e5aab111416e50a1f47d7052bc5f9cab57
帆 张 <932645619@qq.com>
2020-03-19 16:09:47 +0800

    新增sql帮助类和错误日志类

---
+++ WriteLog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+
+namespace WebService
+{
+    public class WriteLog
+    {
+        static string path = "./Log";
+
+        
+       
+
+        public static bool WriteErroLog(string errinfo)
+        {
+            bool result = false;
+            string strException = string.Empty;
+            string filename = "Log" + DateTime.Now.ToString("YYYYMMDD")+".log";
+            string filepath = path + "\\" + filename;
+            try
+            {
+                if (!File.Exists(filepath))
+                    File.Create(filepath);
+                StreamWriter writer = File.AppendText(filepath);
+                writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ":" + errinfo);
+                writer.Flush();
+                writer.Close();
+            }
+            catch(Exception e)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
